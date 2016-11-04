8f6ba85a148469fa13b4a43cb514a3e183f8a223
Lionel Mann <lionelmann@gmail.com>
2016-11-04 09:52:24 -0400

    hooked up wp

---
+++ custom/themes/revive/front-page.php
@@ -0,0 +1,78 @@
+<?php get_header(); ?>
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center blue-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Lorem ipsum dolor</h5>
+
+            <p class="light">Mauris mollis, dui ac feugiat posuere, est turpis aliquet erat, vitae blandit justo ligula sit amet arcu.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center blue-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Lorem ipsum dolor</h5>
+
+            <p class="light">Mauris mollis, dui ac feugiat posuere, est turpis aliquet erat, vitae blandit justo ligula sit amet arcu.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center blue-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Lorem ipsum dolor</h5>
+
+            <p class="light">Mauris mollis, dui ac feugiat posuere, est turpis aliquet erat, vitae blandit justo ligula sit amet arcu.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="<?php echo get_template_directory_uri();?>/dist/images/background2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contact Us</h4>
+          <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="<?php echo get_template_directory_uri();?>/dist/images/background3.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+
+    
+<?php get_footer(); ?>
--- custom/themes/revive/header.php
+++ custom/themes/revive/header.php
@@ -10,11 +10,11 @@
 <body>
   <nav class="white" role="navigation">
     <div class="nav-wrapper container">
-      <a id="logo-container" href="#" class="brand-logo">Logo</a>
+      <a id="logo-container" href="/" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Services</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/services">Services</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
