6b7a0d15104af9b03f294f7e1e0c2c4dfca52b80
Lionel Mann <lionelmann@gmail.com>
2017-01-15 21:46:25 -0500

    images

--- custom/themes/revive/template-services.php
+++ custom/themes/revive/template-services.php
@@ -21,60 +21,40 @@ get_header(); ?>
     </div>
 </div>
 
-
-<!--<div class="flex-grid">-->
-
-
 <div class="service">
-<?php // Loop starts
-$query = array(
-    'post_type' => 'service',
-    'orderby' => 'menu_order',
-    'order'   => 'ASC',
-    );
+    <?php 
 
-query_posts($query); ?>
+    $query = array(
+        'post_type' => 'service',
+        'orderby' => 'menu_order',
+        'order'   => 'ASC',
+        );
 
+    query_posts($query); 
 
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+        $content    = get_the_content();
+        $image      = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); 
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-    $content = get_the_content();
-?>
+    ?>
 
-
-<section class="panel sub-panel wow fadeIn">
-        <span class="table-cell cover panel" style="background-image: url('<?php echo $thumbnail[0]; ?>');">
-
+    <section class="panel sub-panel wow fadeIn">
+        <span class="table-cell cover panel" style="background-image: url('<?php echo $image[0]; ?>');">
             <h4 class="white-text center" ><?php the_title();?></h4>
         </span>
         <div class="table-cell">
             <div class="row">
             <div class="col s12 m8 offset-m2 l9 offset-l1">
-            
             <p style="font-size: 1.5em;"><?php echo $content; ?></p>
             </div>
             </div>
         </div>
     </section>
 
-<!--
-<div class="flex-item">
-    <div class="card">
-        <div class="card-image">
-            <a href="#"><img class="responsive-img" src="<?php //echo $thumbnail[0]; ?>"></a>
-            <span class="card-title"><?php //echo the_title();?></span>
-        </div>
-        <div class="card-content">
-            <p><?php //echo $content;?></p>
-        </div>
-    </div>
-</div>
--->
-<?php endwhile; else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+    <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
 
 </div>
-
 
 <?php get_footer(); ?>
